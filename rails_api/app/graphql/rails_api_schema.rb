1d27207c3873ba29f665bb6825074743c9cc440b
Brian Tonnies <brtonnies@gmail.com>
2019-06-23 16:16:55 -0500

    added subscription type to graphql types, updated graphql schema to use action cable, added callback to model for new links

--- rails_api/app/graphql/rails_api_schema.rb
+++ rails_api/app/graphql/rails_api_schema.rb
@@ -1,4 +1,6 @@
 class RailsApiSchema < GraphQL::Schema
+	use GraphQL::Subscriptions::ActionCableSubscription, redis: Redis.new
   mutation(Types::MutationType)
   query(Types::QueryType)
+  subscription(Types::SubscriptionType)
 end
--- rails_api/app/models/link.rb
+++ rails_api/app/models/link.rb
@@ -1,2 +1,9 @@
 class Link < ApplicationRecord
+	after_save :notify_subscriber_of_addition
+
+	private
+
+	def notify_subscriber_of_addition
+		GraphqlRailsApiSchema.subscription.trigger("newLink", {}, self)
+	end
 end
